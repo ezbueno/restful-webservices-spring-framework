50d6e1687049a9e24678a0d8ac47340ba4f3305b
ezbueno <ezandrobueno@yahoo.com.br>
2022-02-07 11:41:37 -0300

    Created RESTful Web Service Endpoints

--- src/main/java/com/buenoezandro/app/ws/ui/controller/UserController.java
+++ src/main/java/com/buenoezandro/app/ws/ui/controller/UserController.java
@@ -1,10 +1,41 @@
 package com.buenoezandro.app.ws.ui.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
-	
+	@GetMapping
+	public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+			@RequestParam(value = "limit", defaultValue = "50") int limit,
+			@RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+		return "get user was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
+	}
+
+	@GetMapping(path = "/{userId}")
+	public String getUser(@PathVariable(value = "userId") Integer id) {
+		return "get user was called with ID: " + id;
+	}
+
+	@PostMapping
+	public String createUser() {
+		return "create user was called";
+	}
+
+	@PutMapping
+	public String updateUser() {
+		return "update user was called";
+	}
+
+	@DeleteMapping
+	public String deletUser() {
+		return "delete user was called";
+	}
 }
